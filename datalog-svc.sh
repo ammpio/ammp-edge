291eb156492101a8cdec7e745f9c79ef91063cee
Svet Bajlekov <svet.bajlekov@eon-offgrid.com>
2017-09-06 16:28:24 -0400

    Service

--- datalog-svc.sh
+++ datalog-svc.sh
@@ -2,7 +2,7 @@
 
 ### BEGIN INIT INFO
 # Provides:          datalog-svc
-# Required-Start:    $remote_fs $syslog $ntp
+# Required-Start:    $remote_fs $syslog $time
 # Required-Stop:     $remote_fs $syslog
 # Default-Start:     2 3 4 5
 # Default-Stop:      0 1 6
