9f35a3dd608e9b44f03870c678b1271c29fdf348
Svet Bajlekov <svet.bajlekov@eon-offgrid.com>
2017-09-06 13:56:19 -0400

    Add logging for stderr and stdout

--- datalog.py
+++ datalog.py
@@ -38,10 +38,22 @@ logger.info('info')
 logger.warn('warn')
 logger.debug('debug')
 
-# # Replace stdout with logging to file at INFO level
-# sys.stdout = MyLogger(logger, logging.INFO)
-# # Replace stderr with logging to file at ERROR level
-# sys.stderr = MyLogger(logger, logging.ERROR)
+# Make a class we can use to capture stdout and sterr in the log
+class MyLogger(object):
+        def __init__(self, logger, level):
+                """Needs a logger and a logger level."""
+                self.logger = logger
+                self.level = level
+
+        def write(self, message):
+                # Only log if there is a message (not just a new line)
+                if message.rstrip() != "":
+                        self.logger.log(self.level, message.rstrip())
+
+# Replace stdout with logging to file at INFO level
+sys.stdout = MyLogger(logger, logging.INFO)
+# Replace stderr with logging to file at ERROR level
+sys.stderr = MyLogger(logger, logging.ERROR)
 
 class DatalogConfig(object):
     params = {}
