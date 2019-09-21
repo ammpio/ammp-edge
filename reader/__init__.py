62615c8ba4eb1f3c545cb2a40b70d1197fed6dc3
Svet Bajlekov <tomatoman@gmail.com>
2019-09-21 19:30:16 +0200

    Remove unneeded (commented out) imports

--- reader/__init__.py
+++ reader/__init__.py
@@ -1,11 +1,4 @@
 
 from reader.get_readings import get_readings
 
-# from reader.modbustcp_reader import Reader as ModbusTCPReader
-# from reader.modbusrtu_reader import Reader as ModbusRTUReader
-# from reader.rawserial_reader import Reader as RawSerialReader
-# from reader.snmp_reader import Reader as SNMPReader
-# from reader.sys_reader import Reader as SysReader
-
-#__all__ = ['get_readings', 'ModbusTCPReader', 'ModbusRTUReader', 'RawSerialReader', 'SNMPReader', 'SysReader']
 __all__ = ['get_readings']
