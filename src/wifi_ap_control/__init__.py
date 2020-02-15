aa22d994608ac421e3a5a7fe6be67ae2c589cb61
Svet Bajlekov <svet@ip-192-168-1-100.eu-central-1.compute.internal>
2020-02-15 09:33:21 +0100

    Add "wifi available" info

--- src/wifi_ap_control/__init__.py
+++ src/wifi_ap_control/__init__.py
@@ -2,12 +2,14 @@ import logging
 from kvstore import KVStore
 from .wifi_ap_snap_ctl import WifiAPSnapCtl
 from time import sleep
+import sys
 
 # Set up logging
 logging.basicConfig(format='%(threadName)s:%(name)s [%(levelname)s] %(message)s', level='INFO')
 logger = logging.getLogger(__name__)
 
 KVS_CONFIG_KEY = 'node:wifi_ap_config'
+KVS_AVAILABLE_KEY = 'node:wifi_ap_available'
 
 
 def initialize(wifi_ap: WifiAPSnapCtl, kvs: KVStore) -> bool:
@@ -26,8 +28,15 @@ def monitor_and_update(wifi_ap: WifiAPSnapCtl, kvs: KVStore) -> None:
 
 
 def main() -> None:
-    wifi_ap = WifiAPSnapCtl()
     kvs = KVStore()
+
+    try:
+        wifi_ap = WifiAPSnapCtl()
+        kvs.set(KVS_AVAILABLE_KEY, True)
+    except Exception as e:
+        logger.warn(f"Exception while setting up Wifi access point control: {type(e).__name__}: {e}")
+        kvs.set(KVS_AVAILABLE_KEY, False)
+        sys.exit(1)
 
     initialize(wifi_ap, kvs)
     monitor_and_update(wifi_ap, kvs)
