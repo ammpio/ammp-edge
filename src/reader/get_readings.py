4297b30ba49aab40617617066850d70274fab510
Juan David Ruiz <juan.ruiz@ammp.io>
2020-08-31 10:49:19 +0200

    divided dev_rdg and readout into 2 functions

--- src/reader/get_readings.py
+++ src/reader/get_readings.py
@@ -76,6 +76,9 @@ def get_readings(node):
 
         dev_rdg[dev_id].append(rdict)
 
+    return dev_rdg
+
+def get_readout(node):
     # 'readout' is a dict formatted for insertion into InfluxDB (with 'time' and 'fields' keys)
     readout = {
         'time': arrow.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ'),
@@ -83,6 +86,8 @@ def get_readings(node):
         'meta': {}
     }
 
+    dev_rdg = get_readings(node)
+    
     try:
         readout['fields']['comms_lggr_snap_rev'] = int(os.getenv('SNAP_REVISION', 0))
     except Exception:
