7c49d128a8d71ec42c92480e3e365478dd6cf67c
Juan David Ruiz <juan.ruiz@ammp.io>
2020-09-02 16:14:25 +0200

    re-format readout with snap, reading_duration and reading_offset in top level

--- src/data_mgmt/datapusher.py
+++ src/data_mgmt/datapusher.py
@@ -91,8 +91,7 @@ class DataPusher(threading.Thread):
                 readout['meta'].update({'config_id': self._node.config.get('config_id', '')})
 
                 # Append offset between time that reading was taken and current time
-                readout['reading_offset'] = int((arrow.utcnow() - arrow.get(readout['time']).total_seconds()
-                                                 - readout['reading_duration']))
+                readout['reading_offset'] = int((arrow.utcnow() - arrow.get(readout['time']).total_seconds() - readout['reading_duration']))
 
                 readout = make_api_payload(readout)
 
