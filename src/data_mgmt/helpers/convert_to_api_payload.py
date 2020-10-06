52e713a5aa6e729599ddccd94e1af037cb48810a
Juan David Ruiz <juan.ruiz@ammp.io>
2020-10-06 11:21:03 +0200

    Logging api payload

--- src/data_mgmt/helpers/convert_to_api_payload.py
+++ src/data_mgmt/helpers/convert_to_api_payload.py
@@ -10,13 +10,14 @@ This module is currently used by the datapusher
 def convert_to_api_payload(readout):
 	logger.debug(f"CONVERT TO API PAYLOAD. READOUT: {readout}")
 	readout = deepcopy(readout)
+	# fields is a dict will all the readings of all devices -- without the device id. This is done for backwards compatibility
 	fields = {}
 	for rdg in readout['device_readings']:
 		# delete device names, maybe a more elegant way ?
 		dev_id = rdg.pop('dev_id', None)
 		fields.update(rdg)
-	logger.debug(f"CONVERT TO API PAYLOAD. Fields: {fields}")
 	readout['device_readings'] = fields
+	logger.debug(f"CONVERT TO API PAYLOAD. Transformed readout1: {readout}")
 	readout['fields'] = readout.pop('device_readings')
 	logger.debug(f"CONVERT TO API PAYLOAD. Transformed readout: {readout}")
 
