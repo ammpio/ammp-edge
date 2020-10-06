a14513bff8029a4ad04fdba697e1e69668786770
Juan David Ruiz <juan.ruiz@ammp.io>
2020-10-06 11:47:01 +0200

    Logging api payload

--- src/data_mgmt/helpers/convert_to_api_payload.py
+++ src/data_mgmt/helpers/convert_to_api_payload.py
@@ -20,7 +20,7 @@ def convert_to_api_payload(readout):
 	readout['fields'].update({"snap_rev": readout['snap_rev']})
 	readout['fields'].update({"reading_duration": readout['reading_duration']})
 	readout['fields'].update({"reading_offset": readout['reading_offset']})
-	for key in ['snap_rev', 'reading_duration', 'reading_offset']:
+	for key in ['device_readings', 'snap_rev', 'reading_duration', 'reading_offset']:
 		readout.pop(key)
 	logger.debug(f"CONVERT TO API PAYLOAD. READOUT: {readout}")
 	return readout
