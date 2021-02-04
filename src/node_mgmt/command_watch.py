3184419db1b6861b12da96d159a47c719289e159
Svet Bajlekov <tomatoman@gmail.com>
2021-02-04 12:29:30 +0100

    Enable shell commands

--- src/node_mgmt/command_watch.py
+++ src/node_mgmt/command_watch.py
@@ -3,16 +3,18 @@ import time
 import threading
 
 from importlib import import_module
+import os
 
 logger = logging.getLogger(__name__)
 
 # Even if this is not explicitly requested, carry out a command check every COMMAND_CHECK_DELAY seconds
 COMMAND_CHECK_DELAY = 900.0
+SHELL_COMMAND_PREFIX = '#'
 
 
 class CommandWatch(threading.Thread):
     """Request command from node if flag is set"""
-    def __init__(self, node): 
+    def __init__(self, node):
         threading.Thread.__init__(self)
         self.name = 'command_watch'
         # Make sure this thread exits directly when the program exits; no clean-up should be required
@@ -36,12 +38,20 @@ class CommandWatch(threading.Thread):
 
                 if command:
                     logger.info(f"Running command: {command}")
-                    # Runs function with command name from .commands module
-                    try:
-                        commod = import_module('.commands', 'node_mgmt')
-                        getattr(commod, command)(self._node)
-                    except Exception:
-                        logger.exception(f"Could not run command {command}")
+
+                    if command.startswith(SHELL_COMMAND_PREFIX):
+                        # Run shell command
+                        try:
+                            os.system(command[1:])
+                        except Exception:
+                            logger.exception(f"Could not run shell command {command}")
+                    else:
+                        # Runs function with command name from .commands module
+                        try:
+                            commod = import_module('.commands', 'node_mgmt')
+                            getattr(commod, command)(self._node)
+                        except Exception:
+                            logger.exception(f"Could not run command {command}")
 
             except Exception:
                 logger.exception("Exception raised in command watch")
