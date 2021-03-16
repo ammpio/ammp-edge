c052283d14d6a84d84c355bdc31c81aa22241d44
Svet Bajlekov <tomatoman@gmail.com>
2021-03-16 22:53:44 +0100

    opt AMMP-1183: autopep8 formatting

--- src/reader/rawudp_reader.py
+++ src/reader/rawudp_reader.py
@@ -13,28 +13,30 @@ MULTICAST_PORT = 9522
 
 class Reader(object):
     def __init__(
-                self,
-                group: str = MULTICAST_GRP,
-                port: int = MULTICAST_PORT,
-                recv_buffer_size: int = DEFAULT_RECV_BUFFER_SIZE,
-                timeout: int = 5,
-                **kwargs
-                ):
+        self,
+        group: str = MULTICAST_GRP,
+        port: int = MULTICAST_PORT,
+        recv_buffer_size: int = DEFAULT_RECV_BUFFER_SIZE,
+        timeout: int = 5,
+        **kwargs
+    ):
 
-        self._group= group
+        self._group = group
         self._port = port
         self._recv_buffer_size = recv_buffer_size
         self._timeout = timeout
 
     def __enter__(self):
 
-        self._conn = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+        self._conn = socket.socket(
+            socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
         self._conn.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self._conn.settimeout(self._timeout)
         try:
             self._conn.bind(("", MULTICAST_PORT))
         except Exception:
-            logger.error('Exception while attempting to create UDP connection:')
+            logger.error(
+                'Exception while attempting to create UDP connection:')
             raise
 
         return self
@@ -50,11 +52,13 @@ class Reader(object):
 
     def read(self, schema, **rdg):
 
-        request = struct.pack("4sl", socket.inet_aton(MULTICAST_GRP), socket.INADDR_ANY)
+        request = struct.pack("4sl", socket.inet_aton(
+            MULTICAST_GRP), socket.INADDR_ANY)
 
         try:
             logger.debug(f"Writing {repr(request)} to UDP port")
-            self._conn.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, request)
+            self._conn.setsockopt(
+                socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, request)
             response = self._conn.recv(self._recv_buffer_size)
             logger.debug(f"Received {repr(response)} from serial port")
 
@@ -63,16 +67,16 @@ class Reader(object):
                 return
 
         except Exception:
-            logger.error(f"Exception while reading response to query {repr(request)}")
+            logger.error(
+                f"Exception while reading response to query {repr(request)}")
             raise
 
         try:
             # Parse the response to obtain the actual values
             em_data = parse_datagram_response(response)
         except Exception:
-            logger.error(f"Exception while processing value from response {repr(response)}")
+            logger.error(
+                f"Exception while processing value from response {repr(response)}")
             raise
 
         return em_data
-
-
