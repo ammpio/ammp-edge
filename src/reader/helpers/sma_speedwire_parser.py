0e71d195171a32bed62b51ca6bb103da5263900b
Svet Bajlekov <tomatoman@gmail.com>
2021-03-17 23:41:40 +0100

    opt AMMP-1183: simplify parser to only get values, but not interpret them

--- src/reader/helpers/sma_speedwire_parser.py
+++ src/reader/helpers/sma_speedwire_parser.py
@@ -1,112 +1,53 @@
 import logging
+from collections import defaultdict
+from typing import Tuple
 
 logger = logging.getLogger(__name__)
 
-"""
-This module is currently used by the rawudp reader.
-"""
-
-# Parsing SMA energy meter protocol
-# Adapted from https://www.sma.de/fileadmin/content/global/Partner/Documents/SMA_Labs/EMETER-Protokoll-TI-en-10.pdf
-
-# specific channels in the datagram for the Energy-meter
-SMA_CHANNELS = {
-    # totals
-    1: ('total_P_consumption', 'W', 'total_E_consumption', 'kWh'),
-    2: ('total_P_supply', 'W', 'total_E_supply', 'kWh'),
-    14: ('frequency', 'Hz'),
-    # phase 1
-    21: ('L1_P_consumption', 'W', 'L1_E_consumption', 'kWh'),
-    22: ('L1_P_supply', 'W', 'L1_E_supply', 'kWh'),
-    31: ('L1_current', 'A'),
-    32: ('L1_voltage', 'V'),
-    # phase 2
-    41: ('L2_P_consumption', 'W', 'L2_E_consumption', 'kWh'),
-    42: ('L2_P_supply', 'W', 'L2_E_supply', 'kWh'),
-    51: ('L2_current', 'A'),
-    52: ('L2_voltage', 'V'),
-    # phase 3
-    61: ('L3_P_consumption', 'W', 'L2_E_consumption', 'kWh'),
-    62: ('L3_P_supply', 'W', 'L2_E_supply', 'kWh'),
-    71: ('L3_current', 'A'),
-    72: ('L3_voltage', 'V'),
-    # common
-    36864: ('speedwire-version', ''),
-}
-CHANNEL_NAME_ACTUAL = 0
-CHANNEL_UNIT_ACTUAL = 1
-CHANNEL_NAME_CUMULATIVE = 2
-CHANNEL_UNIT_CUMULATIVE = 3
-
-# unit definitions with scaling
-SMA_UNIT_CONVERSION = {
-    'W': 10,
-    'VA': 10,
-    'VAr': 10,
-    'kWh': 3600000,
-    'kVAh': 3600000,
-    'kVArh': 3600000,
-    'A': 1000,
-    'V': 1000,
-    'Hz': 1000,
-}
-
 BYTEORDER_BIG = 'big'
 
-DATATYPE_ACTUAL = 'actual'
-DATATYPE_CUMULATIVE = 'cumulative'
-DATATYPE_VERSION = 'version'
-DATATYPE_UNKNOWN = 'unknown'
+OBIS_TYPE_ACTUAL = 4
+OBIS_TYPE_COUNTER = 8
+OBIS_TYPE_VERSION = 0
+OBIS_CHANNEL_VERSION = 36864
 
 
-def decode_OBIS(obis: bytes) -> tuple:
-    measurement = int.from_bytes(obis[0:2], byteorder=BYTEORDER_BIG)
-    raw_type = int.from_bytes(obis[2:3], byteorder=BYTEORDER_BIG)
-    if raw_type == 4:
-        datatype = DATATYPE_ACTUAL
-    elif raw_type == 8:
-        datatype = DATATYPE_CUMULATIVE
-    elif raw_type == 0 and measurement == 36864:
-        datatype = DATATYPE_VERSION
-    else:
-        datatype = DATATYPE_UNKNOWN
-        logger.error(
-            f"unknown datatype: measurement {measurement} raw_type {raw_type}")
-    return measurement, datatype
+def decode_obis(obis: bytes) -> Tuple[int, int]:
+    obis_channel = int.from_bytes(obis[0:2], byteorder=BYTEORDER_BIG)
+    obis_type = int.from_bytes(obis[2:3], byteorder=BYTEORDER_BIG)
+    return obis_channel, obis_type
 
 
-def parse_datagram_response(response: bytes) -> tuple:
-    values = {}
+def parse_datagram_response(response: bytes) -> Tuple[int, dict]:
     data_length = int.from_bytes(response[12:14], byteorder=BYTEORDER_BIG) + 16
     serial_number = int.from_bytes(response[20:24], byteorder=BYTEORDER_BIG)
+
+    # top-level key is channel, second-level key is type
+    values = defaultdict(dict)
 
     # initial position for relevant data in datagram
     position = 28
     while position < data_length:
-        (measurement, datatype) = decode_OBIS(response[position:position + 4])
+        (obis_channel, obis_type) = decode_obis(
+            response[position:position + 4])
         # spot values
-        if datatype == DATATYPE_ACTUAL:
-            value = int.from_bytes(
-                response[position + 4:position + 8], byteorder=BYTEORDER_BIG)
+        if obis_type == OBIS_TYPE_ACTUAL:
+            value = response[position + 4:position + 8]
             position += 8
-            if measurement in SMA_CHANNELS:
-                channel_spec = SMA_CHANNELS[measurement]
-                conversion_factor = SMA_UNIT_CONVERSION[channel_spec[CHANNEL_UNIT_CUMULATIVE]]
-                values[channel_spec[CHANNEL_NAME_ACTUAL]
-                       ] = value / conversion_factor
-
         # counter values
-        elif datatype == DATATYPE_CUMULATIVE:
-            value = int.from_bytes(
-                response[position + 4:position + 12], byteorder=BYTEORDER_BIG)
+        elif obis_type == OBIS_TYPE_COUNTER:
+            value = response[position + 4:position + 12]
             position += 12
-            if measurement in SMA_CHANNELS:
-                channel_spec = SMA_CHANNELS[measurement]
-                conversion_factor = SMA_UNIT_CONVERSION[channel_spec[CHANNEL_UNIT_CUMULATIVE]]
-                values[channel_spec[CHANNEL_NAME_CUMULATIVE]
-                       ] = value / conversion_factor
-
-        elif datatype == DATATYPE_VERSION:
+        # version value
+        elif obis_type == OBIS_TYPE_VERSION and obis_channel == OBIS_CHANNEL_VERSION:
             position += 8
+            value = response[position + 4:position + 8]
+        else:
+            logger.error(
+                f"Cannot parse OBIS channel and type {obis_channel}.{obis_type}; stopping parse")
+            value = None
+            break
 
-    return serial_number, values
+        values[obis_channel][obis_type] = value
+
+    return serial_number, dict(values)
