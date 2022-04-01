c692dafc028faff030ebc42e469fef1e9550dce3
Svet Bajlekov <tomatoman@gmail.com>
2022-04-01 18:24:40 +0100

    test AMMP-2194: add integration test

---
+++ rust/kvstore/tests/integration_test.rs
@@ -0,0 +1,40 @@
+use anyhow::Result;
+use kvstore::{DbRO, DbRW};
+use serde_derive::{Deserialize, Serialize};
+use std::fs;
+
+const SQLITE_DIR: &str = "testdb_temp";
+const SQLITE_FILE: &str = "kvstore.db";
+
+#[derive(Debug, Deserialize, Serialize, PartialEq)]
+pub struct Cat {
+    name: String,
+    lives: u64,
+    siblings: Vec<String>,
+}
+
+#[test]
+fn write_and_read_object() -> Result<()> {
+    env_logger::init();
+    let sqlite_db = format!("{}/{}", SQLITE_DIR, SQLITE_FILE);
+
+    let db = DbRW::open(&sqlite_db)?;
+
+    let lulu = Cat {
+        name: String::from("Lulu"),
+        lives: 9,
+        siblings: vec![String::from("Mollie"), String::from("Lilly")],
+    };
+
+    db.set("lulu", &lulu)?;
+    let lulu2: Cat = db.get("lulu").expect("Error reading KV store").unwrap();
+    assert_eq!(lulu2, lulu);
+
+    let db2 = DbRO::open(&sqlite_db)?;
+    let lulu3: Cat = db2.get("lulu").expect("Error reading KV store").unwrap();
+    assert_eq!(lulu3, lulu);
+
+    log::info!("Deleting DB directory {SQLITE_DIR}");
+    fs::remove_dir_all(SQLITE_DIR)?;
+    Ok(())
+}
