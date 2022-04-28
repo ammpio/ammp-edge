5a003825c1a67e6094da1a3b8c24055811df154d
Svet Bajlekov <tomatoman@gmail.com>
2022-04-28 16:58:42 +0200

    feat AMMP-2194: better setting of log level

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,17 +1,22 @@
 mod argsets;
 mod command;
 mod helpers;
-mod node_mgmt;
 mod interfaces;
+mod node_mgmt;
 
 use anyhow::{anyhow, Result};
+use env_logger::Env;
 
 const CMD_INIT: &str = "init";
 const CMD_KVS_GET: &str = "kvs-get";
 const CMD_KVS_SET: &str = "kvs-set";
 
+const LOG_LEVEL_ENV_VAR: &str = "LOGGING_LEVEL";
+const DEFAULT_LOG_LEVEL: &str = "info";
+
 fn main() -> Result<()> {
-    env_logger::init();
+    env_logger::Builder::from_env(Env::default().filter_or(LOG_LEVEL_ENV_VAR, DEFAULT_LOG_LEVEL))
+        .init();
 
     let mut args = pico_args::Arguments::from_env();
     match args.subcommand()?.as_deref() {
@@ -23,6 +28,8 @@ fn main() -> Result<()> {
             key: args.free_from_str()?,
             value: args.free_from_str()?,
         }),
-        _ => Err(anyhow!("Subcommand must be one of 'init', 'kvs-get', 'kvs-set'")),
+        _ => Err(anyhow!(
+            "Subcommand must be one of 'init', 'kvs-get', 'kvs-set'"
+        )),
     }
 }
