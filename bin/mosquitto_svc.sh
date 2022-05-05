54d39974967db1ae237df8c9cfbe773d2d77556c
Svet Bajlekov <svet.bajlekov@ammp.io>
2022-05-05 14:44:08 +0200

    naming updates

--- bin/mosquitto_svc.sh
+++ bin/mosquitto_svc.sh
@@ -9,18 +9,18 @@ mkdir -p $MOSQUITTO_DIR $INCLUDE_DIR
 # Main config
 envsubst < $SNAP/mosquitto.conf.tpl > $CONFIG_FILE
 
-export NODE_ID=$(ammp-kvs get node_id)
-export ACCESS_KEY=$(ammp-kvs get access_key)
+export NODE_ID=$(ae kvs-get node_id)
+export ACCESS_KEY=$(ae kvs-get access_key)
 
 # Bridge to prod broker - enable unless explicitly disabled
-if [ $(ammp-kvs get mqtt_bridge) = 'false' ]; then
+if [ $(ae kvs-get mqtt_bridge) = 'false' ]; then
   rm -f $INCLUDE_DIR/mqtt-bridge.conf
 else
   envsubst < $SNAP/mqtt-bridge.conf.tpl > $INCLUDE_DIR/mqtt-bridge.conf
 fi
 
 # Bridge to stage broker - disable unless explicitly enabled
-if [ $(ammp-kvs get mqtt_bridge_stage) = 'true' ]; then
+if [ $(ae kvs-get mqtt_bridge_stage) = 'true' ]; then
   envsubst < $SNAP/mqtt-bridge-stage.conf.tpl > $INCLUDE_DIR/mqtt-bridge-stage.conf
 else
   rm -f $INCLUDE_DIR/mqtt-bridge-stage.conf
