7c7411b5c39472aae815fc79ed2ca53cc568225a
Svet Bajlekov <svet.bajlekov@ammp.io>
2022-05-09 15:29:57 +0200

    don't print log messages about .env (confuses kvs-get)

--- rust/src/helpers/load_dotenv.rs
+++ rust/src/helpers/load_dotenv.rs
@@ -1,13 +1,8 @@
 use crate::helpers::base_path;
 
 pub fn load_dotenv() {
-    if dotenv::dotenv().is_ok() {
-        println!("Loaded local .env")
-    }
+    dotenv::dotenv().ok();
 
-    let data_dir_dotenv = format!("{}/.env", base_path::data_dir());
     // Also load .env from data dir if exists
-    if dotenv::from_path(&data_dir_dotenv).is_ok() {
-        println!("Loaded {data_dir_dotenv}");
-    }
+    dotenv::from_path(&format!("{}/.env", base_path::data_dir())).ok();
 }
