e5193f5430f4fc368ce9184509eda363ea8ceefb
Svet Bajlekov <svet.bajlekov@ammp.io>
2022-05-09 22:49:56 +0200

    fix AMMP-2194: correct checks for MQTT bridge on/off

--- bin/mosquitto_svc.sh
+++ bin/mosquitto_svc.sh
@@ -13,14 +13,14 @@ export NODE_ID=$(ae kvs-get node_id)
 export ACCESS_KEY=$(ae kvs-get access_key)
 
 # Bridge to prod broker - enable unless explicitly disabled
-if [ $(ae kvs-get mqtt_bridge) = 'false' ]; then
+if [ "$(ae kvs-get mqtt_bridge)" = 'false' ]; then
   rm -f $INCLUDE_DIR/mqtt-bridge.conf
 else
   envsubst < $SNAP/mqtt-bridge.conf.tpl > $INCLUDE_DIR/mqtt-bridge.conf
 fi
 
 # Bridge to stage broker - disable unless explicitly enabled
-if [ $(ae kvs-get mqtt_bridge_stage) = 'true' ]; then
+if [ "$(ae kvs-get mqtt_bridge_stage)" = 'true' ]; then
   envsubst < $SNAP/mqtt-bridge-stage.conf.tpl > $INCLUDE_DIR/mqtt-bridge-stage.conf
 else
   rm -f $INCLUDE_DIR/mqtt-bridge-stage.conf
