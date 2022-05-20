57b399febddb1375e2af1a5dcfda541ad0b9b527
Svet Bajlekov <tomatoman@gmail.com>
2022-05-20 17:18:31 +0200

    clean up

--- rust/src/interfaces/kvpath.rs
+++ rust/src/interfaces/kvpath.rs
@@ -11,8 +11,3 @@ pub static SQLITE_STORE: Lazy<PathBuf> = Lazy::new(||
 pub static SQLITE_CACHE: Lazy<PathBuf> = Lazy::new(||
     base_path::TEMP_DIR.join("ae-kvcache.db")
 );
-
-// lazy_static! {
-//     pub static ref SQLITE_STORE: PathBuf = base_path::DATA_DIR.join("kvs-db/kvstore.db");
-//     pub static ref SQLITE_CACHE: PathBuf = base_path::TEMP_DIR.join("ae-kvcache.db");
-// }
