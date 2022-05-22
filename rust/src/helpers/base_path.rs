c19ab89cc6453c4852a226d0f579331dbb16f82c
Svet Bajlekov <tomatoman@gmail.com>
2022-05-22 20:12:47 +0200

    test AMMP-2194: add tests for base_path

--- rust/src/helpers/base_path.rs
+++ rust/src/helpers/base_path.rs
@@ -31,3 +31,54 @@ pub static TEMP_DIR: Lazy<PathBuf> = Lazy::new(|| {
     }
     PathBuf::from("/tmp")
 });
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const SAMPLE_ROOT_DIR: &str = "/opt/ae";
+    const SAMPLE_DATA_DIR: &str = "/opt/ae/data";
+    const SAMPLE_TEMP_DIR: &str = "/opt/ae/tmp";
+
+    #[test]
+    fn with_ae_vars_set() {
+        temp_env::with_vars(
+            vec![
+                (envvars::ROOT_DIR, Some(SAMPLE_ROOT_DIR)),
+                (envvars::DATA_DIR, Some(SAMPLE_DATA_DIR)),
+                (envvars::TEMP_DIR, Some(SAMPLE_TEMP_DIR)),
+            ],
+            || {
+                assert_eq!(ROOT_DIR.as_os_str(), SAMPLE_ROOT_DIR);
+                assert_eq!(DATA_DIR.as_os_str(), SAMPLE_DATA_DIR);
+                assert_eq!(TEMP_DIR.as_os_str(), SAMPLE_TEMP_DIR);
+            }
+        );
+    }
+
+    // Only one test can be run at a time - since once the value
+    // of any of the Lazy statics is set, it will remain the same
+    // while the other tests are run (which will probably fail)
+    #[test]
+    #[ignore]
+    fn with_snap_vars_set() {
+        temp_env::with_vars(
+            vec![
+                (envvars::SNAP, Some(SAMPLE_ROOT_DIR)),
+                (envvars::SNAP_COMMON, Some(SAMPLE_DATA_DIR)),
+            ],
+            || {
+                assert_eq!(ROOT_DIR.as_os_str(), SAMPLE_ROOT_DIR);
+                assert_eq!(DATA_DIR.as_os_str(), SAMPLE_DATA_DIR);
+            }
+        );
+    }
+
+    #[test]
+    #[ignore]
+    fn without_vars_set() {
+        assert_eq!(ROOT_DIR.as_os_str(), ".");
+        assert_eq!(DATA_DIR.as_os_str(), "./data");
+        assert_eq!(TEMP_DIR.as_os_str(), "/tmp");
+    }
+}
