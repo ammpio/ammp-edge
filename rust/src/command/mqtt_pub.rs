82b3b636685706ad334dd91f98ececa1501a0fbd
Svet Bajlekov <tomatoman@gmail.com>
2022-09-22 18:50:59 +0200

    AMMP-2905: publish snap revision

--- rust/src/command/mqtt_pub.rs
+++ rust/src/command/mqtt_pub.rs
@@ -1,20 +1,15 @@
+use std::env;
+
 use anyhow::Result;
 
+use crate::envvars::SNAP_REVISION;
 use crate::interfaces::mqtt::{self, MqttMessage};
 
 pub fn mqtt_pub_meta() -> Result<()> {
     let messages = vec!(
         MqttMessage {
-            topic: "u/meta/snap1".into(),
-            payload: "abc".into(),
-        },
-        MqttMessage {
-            topic: "u/meta/snap2".into(),
-            payload: "abc1".into(),
-        },
-        MqttMessage {
-            topic: "u/meta/snap3".into(),
-            payload: "abc2".into(),
+            topic: "u/meta/snap_rev".into(),
+            payload: env::var(SNAP_REVISION).unwrap_or_else(|_| "N/A".into()),
         },
     );
     mqtt::publish_msgs(messages, Some(true))
