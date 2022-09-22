580251ac57507b620e850501168587ce7e89aa5b
Svet Bajlekov <tomatoman@gmail.com>
2022-09-22 23:22:54 +0200

    AMMP-2905: publish boot time

--- rust/src/command/mqtt_pub.rs
+++ rust/src/command/mqtt_pub.rs
@@ -1,6 +1,7 @@
 use std::env;
 
 use anyhow::Result;
+use sys_info::boottime;
 
 use crate::envvars::SNAP_REVISION;
 use crate::interfaces::mqtt::{self, MqttMessage};
@@ -10,6 +11,10 @@ pub fn mqtt_pub_meta() -> Result<()> {
         MqttMessage {
             topic: "u/meta/snap_rev".into(),
             payload: env::var(SNAP_REVISION).unwrap_or_else(|_| "N/A".into()),
+        },
+        MqttMessage {
+            topic: "u/meta/boottime".into(),
+            payload: boottime().unwrap().tv_sec.to_string(),
         },
     );
     mqtt::publish_msgs(messages, Some(true))
