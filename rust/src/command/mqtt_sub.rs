f7fee60a903853238fea5936fcf4a8d1f6cb8b12
Svet Bajlekov <tomatoman@gmail.com>
2022-10-19 00:35:40 +0200

    AMMP-2910: improved error handling and retry on MQTT disconnect

--- rust/src/command/mqtt_sub.rs
+++ rust/src/command/mqtt_sub.rs
@@ -1,4 +1,3 @@
-use anyhow::Result;
 use kvstore::KVDb;
 
 use crate::constants::topics;
@@ -8,20 +7,24 @@ use crate::interfaces::mqtt::{sub_topics, MqttMessage};
 use crate::node_mgmt;
 
 fn try_set_config(config_payload: String) {
-    // An invalid config would lead to a permanent error that is not retried
-    // A databse connection or write error is transient and would lead toa retry
-    // TODO: Set maximum number of retries, once https://github.com/ihrwein/backoff/pull/60 is merged 
-    let set_config = || {
-        let config = node_mgmt::config::from_string(&config_payload)
-            .map_err(Into::into)
-            .map_err(backoff::Error::permanent)?;
-        let kvs = KVDb::new(kvpath::SQLITE_STORE.as_path()).map_err(backoff::Error::transient)?;
-        node_mgmt::config::set(kvs, config).map_err(backoff::Error::transient)
-    };
+    if let Ok(config) = node_mgmt::config::from_string(&config_payload) {
+        // A databse connection or write error is transient and would lead toa retry
+        // TODO: Set maximum number of retries, once https://github.com/ihrwein/backoff/pull/60 is merged
+        let set_config = || {
+            let kvs = KVDb::new(kvpath::SQLITE_STORE.as_path())?;
+            node_mgmt::config::set(kvs, config.clone())?;
+            Ok(())
+        };
 
-    match backoff_retry(set_config) {
-        Ok(()) => log::info!("Successfully set new config"),
-        Err(err) => log::error!("Permanent error setting config: {:?}", err),
+        match backoff_retry(set_config) {
+            Ok(()) => log::info!("Successfully set new config"),
+            Err(e) => log::error!("Permanent error setting config: {:?}", e),
+        }
+    } else {
+        log::error!(
+            "Could not parse received payload as valid config: {:?}",
+            &config_payload
+        );
     }
 }
 
@@ -34,10 +37,15 @@ fn process_msg(msg: MqttMessage) {
     }
 }
 
-pub fn mqtt_sub_cfg() -> Result<()> {
-    sub_topics(
-        &[topics::CONFIG.into(), topics::COMMAND.into()],
-        Some("local-sub-cfg".into()),
-        process_msg,
-    )
+pub fn mqtt_sub_cfg() -> anyhow::Result<()> {
+    let sub_loop = || {
+        sub_topics(
+            &[topics::CONFIG.into(), topics::COMMAND.into()],
+            Some("local-sub-cfg".into()),
+            process_msg,
+        )
+        .map_err(backoff::Error::transient)
+    };
+    backoff_retry(sub_loop)?;
+    Ok(())
 }
