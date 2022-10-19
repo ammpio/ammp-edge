6fc9a597802fa909e4508a8346d713f8e8c0e707
Svet Bajlekov <tomatoman@gmail.com>
2022-10-19 09:59:14 +0200

    Backoff should retry forever rather than give up after 15 minutes

--- rust/src/helpers/backoff_retry.rs
+++ rust/src/helpers/backoff_retry.rs
@@ -1,7 +1,7 @@
 use std::fmt::Display;
 use std::time::Duration;
 
-use backoff::{retry_notify, Error, ExponentialBackoff};
+use backoff::{retry_notify, Error, ExponentialBackoff, ExponentialBackoffBuilder};
 
 pub fn backoff_retry<F, T, E>(fn_to_try: F) -> Result<T, Error<E>>
 where
@@ -12,5 +12,8 @@ where
         log::error!("Temporary error after {:.1}s: {}", dur.as_secs_f32(), err);
     };
 
-    retry_notify(ExponentialBackoff::default(), fn_to_try, notify)
+    // Set to retry forever, rather than give up after 15 minutes.
+    // See https://github.com/ihrwein/backoff/issues/39
+    let backoff = ExponentialBackoffBuilder::new().with_max_elapsed_time(None).build();
+    retry_notify(backoff, fn_to_try, notify)
 }
