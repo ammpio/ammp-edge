0bd7d43f87c8088a7ad7887b3e72bbb2c3875675
Svet Bajlekov <tomatoman@gmail.com>
2022-11-06 19:36:53 +0100

    define MQTT publish topics as constants

--- rust/src/command/mqtt_pub.rs
+++ rust/src/command/mqtt_pub.rs
@@ -4,6 +4,7 @@ use std::time::Duration;
 use anyhow::Result;
 use sysinfo::{System, SystemExt};
 
+use crate::constants::topics;
 use crate::envvars::SNAP_REVISION;
 use crate::helpers::{backoff_retry, get_node_arch, get_ssh_fingerprint, now_epoch};
 use crate::interfaces::mqtt::{publish_msgs, MqttMessage};
@@ -13,30 +14,30 @@ const PUBLISH_TIMEOUT: Duration = Duration::from_secs(30);
 fn construct_meta_msg() -> Vec<MqttMessage> {
     let mut msgs = vec![
         MqttMessage {
-            topic: "u/meta/boot_time".into(),
+            topic: topics::META_BOOT_TIME.to_string(),
             payload: System::new().boot_time().to_string(),
         },
         MqttMessage {
-            topic: "u/meta/start_time".into(),
+            topic: topics::META_START_TIME.to_string(),
             payload: now_epoch().to_string(),
         },
     ];
 
     if let Ok(snap_revision) = env::var(SNAP_REVISION) {
         msgs.push(MqttMessage {
-            topic: "u/meta/snap_rev".into(),
+            topic: topics::META_SNAP_REV.to_string(),
             payload: snap_revision,
         })
     }
     if let Ok(arch) = get_node_arch() {
         msgs.push(MqttMessage {
-            topic: "u/meta/arch".into(),
+            topic: topics::META_ARCH.to_string(),
             payload: arch,
         })
     }
     if let Ok(ssh_fingerprint) = get_ssh_fingerprint() {
         msgs.push(MqttMessage {
-            topic: "u/meta/ssh_fingerprint".into(),
+            topic: topics::META_SSH_FINGERPRINT.to_string(),
             payload: ssh_fingerprint,
         });
     }
