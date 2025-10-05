3f0aac9acd4f172ef87300f1a1a98b092a3f75ab
Svet Bajlekov <tomatoman@gmail.com>
2025-10-04 23:03:54 -0400

    only pass reference to kvs for getting/setting config

--- rust/src/command/sma_hycon_csv.rs
+++ rust/src/command/sma_hycon_csv.rs
@@ -8,7 +8,7 @@ const DATA_PROVIDER: &str = "sma-hycon-csv";
 
 pub fn read_sma_hycon_csv() -> anyhow::Result<()> {
     let kvs = KVDb::new(kvpath::SQLITE_STORE.as_path())?;
-    let config = node_mgmt::config::get(kvs)?;
+    let config = node_mgmt::config::get(&kvs)?;
     let readings = readers::sma_hycon_csv::run_acquisition(&config);
     log::info!(
         "Finished SMA Hycon CSV downloads; obtained {} readings",
